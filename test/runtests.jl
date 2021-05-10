51cbc1d65dfdf61e3666f08581af56b8761a9d47
kai <supon3060@gmail.com>
2021-05-10 16:03:29 +0800

    Add test

---
+++ test/runtests.jl
@@ -0,0 +1,26 @@
+using Test
+using Random
+using Statistics
+using InformationAnalysis
+
+Random.seed!(1234)
+μ = 0
+rtol = 0.03
+theoretical_fim(σ) = 1 / σ^2
+theoretical_se(σ) = 1 / 2 * log(2 * π * ℯ * σ^2)
+
+@testset verbose = true "All Information Metrics(σ=$σ)" for σ in range(0.1; stop=5.9, step=0.1)
+    data = σ * randn(Float64, 100000) .+ μ
+
+    @testset "FIM (σ=$σ)" begin
+        fim = fisherinformation(data)
+        @test isapprox(fim, theoretical_fim(σ); rtol=rtol)
+        @test abs(fim - theoretical_fim(σ)) / abs(theoretical_fim(σ)) <= rtol
+    end
+
+    @testset "SE (σ=$σ)" begin
+        se = shannonentropy(data)[1]
+        @test isapprox(se, theoretical_se(σ); rtol=rtol)
+        @test abs(se - theoretical_se(σ)) / abs(theoretical_se(σ)) <= rtol
+    end
+end
