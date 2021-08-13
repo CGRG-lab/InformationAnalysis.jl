8cf51c988f04863624392dc7bbefbbf66d1a8d2c
kai <supon3060@gmail.com>
2021-08-13 16:30:24 +0800

    ♻️ Make type assertion more general and rename variable

--- src/fisher_information.jl
+++ src/fisher_information.jl
@@ -35,21 +35,21 @@ function fisherinformation(data::AbstractArray{Float64})
 end
 
 """
-    shannonentropy(data::AbstractArray{Float64}, l::Int64)
+    shannonentropy(data::AbstractArray{Float64}, batch_size::Integer)
 
 Sequentially use small batch data to calculate "Fisher Information" (∫ ds/p(s) [dp(s)/ds]²)
 """
-function fisherinformation(data::AbstractArray{Float64}, l::Int64)
+function fisherinformation(data::AbstractArray{Float64}, batch_size::Integer)
     num_data = length(data)
-    l > num_data && throw(BoundsError("window length $l > $(num_data)"))
+    batch_size > num_data && throw(BoundsError("Batch size length $batch_size > $(num_data)"))
     if ndims(data) != 1
         @debug "Flatten data along first axis."
         data = data[:]
     end
     
-    num_fi = num_data - l + 1
+    num_fi = num_data - batch_size + 1
     fi = Array{Float64,1}(undef, num_fi)
-    index = Array(1:num_fi) .+ (l - 1)
+    index = Array(1:num_fi) .+ (batch_size - 1)
     @batch per = core for i in 1:num_fi
         # method 1 (BSpline interpolation may give negative value (overshoot))
         # ik = InterpKDE(kde(data[i:(i + l - 1)]))
@@ -58,7 +58,7 @@ function fisherinformation(data::AbstractArray{Float64}, l::Int64)
 
         # method 2 (Monotonic interpolation may give 0)
         # https://discourse.julialang.org/t/interpolations-jl-discrete-cdf-to-pdf/60124/8
-        _kde = kde(data[i:(i + l - 1)])
+        _kde = kde(data[i:(i + batch_size - 1)])
         itp = interpolate(_kde.x, _kde.density, SteffenMonotonicInterpolation())
         etp = extrapolate(itp, zero(eltype(_kde.density)))
         ik = InterpKDE{typeof(_kde),typeof(etp)}(_kde, etp)
